af99678b5dd6979e6017c89f0b40287921e2861c
Julen <julenpe514@gmail.com>
2026-02-06 14:36:03 +0100

    cd avanzado, notas.txt y apuntes.txt anadido

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,29 +6,29 @@
 /*   By: julepere <julepere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 17:09:19 by julepere          #+#    #+#             */
-/*   Updated: 2026/02/06 12:48:03 by julepere         ###   ########.fr       */
+/*   Updated: 2026/02/06 13:07:23 by julepere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int xy_cd(t_command *cmd, t_shell *sh)
+int	xy_cd(t_command *cmd, t_shell *sh)
 {
-	char path;
+    const char	*path;
 
-	path = cmd->argv[1];
-	if (chdir(path))
-	{
+    if (!sh)
+        return (0);
+    sh->exit_status = 0;
+    if (!cmd || !cmd->argv || !cmd->argv[1])
+        return (1);
+    path = cmd->argv[1];
+    if (chdir(path) != 0)
+    {
 		write(1, "se ha cambiado a ", 17);
 		write(1, path, xy_strlen(path));
-	
-		sh->exit_status = 0;
-		chdir(path);
-	}
-	else
-	{
-		write(1, "ruta no cambiada", 16);
-		sh->exit_status = 1;
-	}
-	return (0);
+        sh->exit_status = 1;
+        return (1);
+    }
+	getenv("HOME");
+    return (0);
 }

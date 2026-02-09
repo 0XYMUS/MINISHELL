0c583687611370ac0439d44dddf8cf569715b97c
julepere <julenpe514@gmail.com>
2026-02-09 21:25:58 +0100

    exit raro, que le pasa.

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: julepere <julepere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 15:31:36 by julepere          #+#    #+#             */
-/*   Updated: 2026/02/09 18:53:36 by julepere         ###   ########.fr       */
+/*   Updated: 2026/02/09 21:24:43 by julepere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 int	xy_exit(t_command *cmd, t_shell *sh)
 {
 	(void)cmd;
-
-	printf("exit\n");
+	if (!sh)
+		return (0);
+	write(1, "exit\n", 5);
 	exit(sh->exit_status);
 }

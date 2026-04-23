08f602014ac00c1bf93a263348267c3fb6bef1cc
jojeda-p <josunom@gmail.com>
2026-04-23 15:02:03 +0200

    correccion de cd

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: jojeda-p <jojeda-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 17:09:19 by julepere          #+#    #+#             */
-/*   Updated: 2026/04/20 17:20:26 by jojeda-p         ###   ########.fr       */
+/*   Updated: 2026/04/23 12:57:26 by jojeda-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,9 @@ int	xy_cd(t_cmd *cmd, t_shell *sh)
 	path = cmd->argv[1];
 	if (chdir(path) != 0)
 	{
-		write(1, "minishell: cd: No existe el archivo o el directorio\n", 52);
+		write(1, "minishell: cd: No such file or directory\n", 41);
 		sh->exit_status = 1;
 		return (1);
 	}
-	write(1, "se ha cambiado a ", 17);
-	write(1, path, ft_strlen(path));
-	write(1, "\n", 1);
 	return (0);
 }
